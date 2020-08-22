cddf8d261fe43c94e16a6b31562f4496752d2988
Sebastian Cristi <grayjacketstudios@gmail.com>
2020-08-21 23:56:10 -0300

    Se elimina paquete app-root-path Se crea la funcion para obtener los nombres de archivo.

--- translate.js
+++ translate.js
@@ -1,22 +1,25 @@
-"use strict";
-
-var main = require('app-root-path')
+"use strict"
 const translationFiles = require('./utils/GetFiles')
 
 
 class Translate {
 
     constructor(){
-        this.translationPathJson = `${main}/data`
-        this.files = null
+        this.defaultLang = "en"
     }
 
-    path(path){
-        this.translationPathJson = path
-    }
 
+    setPath(path){
+        this.translationPath = path
+    }
+    setDefaultLang(lang){
+        this.defaultLang = lang
+    }
     async getFiles(){
-        this.files = await translationFiles(this.translationPathJson)
+        if(this.translationPath == null){
+            throw Error("Translation PATH dont defined.")
+        }
+        this.files = await translationFiles(this.translationPath)
         console.log(this.files)
     }
 }
--- utils/GetFiles.js
+++ utils/GetFiles.js
@@ -5,14 +5,29 @@ const util = require('util');
 
 const readdir = util.promisify(fs.readdir);
 
-module.exports = async function (path) {
-  let res = await readdir(path, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    return files
+module.exports = (path) => {
+  return new Promise((resolve, reject) => {
+    readdir(path, (err, files) => {
+      if(err) reject(err)
+      if(files.length == 0)
+        reject("Empty translation directory")
+      else {
+        resolve(files)
+      }
+    });
   });
-  return res
 }
+
+
+
+// async function (path) {
+//   let res = await readdir(path, function (err, files) {
+//     //handling error
+//     if (err) {
+//         return console.log('Unable to scan directory: ' + err);
+//     }
+//     //listing all files using forEach
+//     return files
+//   });
+//   return res
+// }
